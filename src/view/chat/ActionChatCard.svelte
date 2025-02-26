74f5babf1135441b1aa701d11997794372ec525c
Phil <me@phillip.best>
2025-02-26 21:19:44 +0000

    Adds a conditional reroll button to the action chat cards.

--- src/view/chat/ActionChatCard.svelte
+++ src/view/chat/ActionChatCard.svelte
@@ -1,27 +1,52 @@
 <script>
-    import getRollResultLabel from "../dataPreparationHelpers/getRollResultLabel.ts";
-    import getRollSummaryLabel from "../dataPreparationHelpers/getRollSummaryLabel.ts";
-    import getSetbackLabel from "../dataPreparationHelpers/getSetbackLabel.ts";
-    import { setContext } from "svelte";
+import getRollResultLabel from '../dataPreparationHelpers/getRollResultLabel.ts';
+import getRollSummaryLabel from '../dataPreparationHelpers/getRollSummaryLabel.ts';
+import getSetbackLabel from '../dataPreparationHelpers/getSetbackLabel.ts';
+import { setContext } from 'svelte';
 
-    import CardHeader from "./components/CardHeader.svelte";
+import CardHeader from './components/CardHeader.svelte';
 
-    const { messageDocument } = $props();
-    const { system, rolls } = messageDocument;
-    const { DIFFICULTY_LEVELS } = CONFIG.BLOOD_AND_DOOM;
-    const difficultyLevel = DIFFICULTY_LEVELS.get(system.difficulty) ?? 0;
+function determineRerollButtonVisibility() {
+	const difficultyThreshold = DIFFICULTY_LEVELS.get(system.difficulty);
 
-    let rollSummary = getRollSummaryLabel(system.successes, difficultyLevel);
-    let rollResult = getRollResultLabel(system.successes, difficultyLevel);
-    let setbackLabel = getSetbackLabel(system.setback, system.successes, difficultyLevel);
+	if (system.rerolled) return false;
+	if (system.setback) return false;
+	if (difficultyThreshold && system.successes >= difficultyThreshold) return false;
 
-    setContext("messageDocument", messageDocument);
+	return true;
+}
+
+const { messageDocument } = $props();
+const { system, rolls } = messageDocument;
+const { DIFFICULTY_LEVELS } = CONFIG.BLOOD_AND_DOOM;
+const difficultyLevel = DIFFICULTY_LEVELS.get(system.difficulty) ?? 0;
+
+let rollSummary = getRollSummaryLabel(system.successes, difficultyLevel);
+let rollResult = getRollResultLabel(system.successes, difficultyLevel);
+let showRerollButton = determineRerollButtonVisibility();
+let setbackLabel = getSetbackLabel(system.setback, system.successes, difficultyLevel);
+
+setContext('messageDocument', messageDocument);
 </script>
 
 <article class="bnd-chat-card__body bnd-chat-card__body--action">
     <CardHeader />
 
-    <h3 class="bnd-heading bnd-heading--chat-title">Action Roll</h3>
+    <h3 class="bnd-heading bnd-heading--chat-title">
+        Action Roll
+
+        {#if showRerollButton}
+            <button
+                class="bnd-roll-button"
+                type="button"
+                aria-label="Expend Blood Point to reroll"
+                data-tooltip="Expend Blood Point to Reroll"
+                onclick={() => messageDocument.reroll()}
+            >
+                <i class="fa-solid fa-dice"></i>
+            </button>
+        {/if}
+    </h3>
 
     <div class="bnd-chat-card__result-wrapper">
         <div class="bnd-chat-card__result">
@@ -29,7 +54,7 @@
         </div>
     </div>
 
-    {#if difficultyLevel || system.setback}
+    {#if difficultyLevel || system.setback || system.rerolled}
         <div class="bnd-chat-card__result-wrapper">
             {#if difficultyLevel}
                 <div
@@ -43,7 +68,7 @@
                 </div>
             {/if}
 
-            {#if difficultyLevel && system.setback}
+            {#if (difficultyLevel && system.setback) || (difficultyLevel && system.rerolled)}
                 <span class="bnd-chat-card__result-divider"> | </span>
             {/if}
 
@@ -54,6 +79,30 @@
                     {setbackLabel}
                 </div>
             {/if}
+
+            {#if system.rerolled}
+                <div
+                    class="bnd-chat-card__result bnd-chat-card__result--uppercase bnd-chat-card__result--rerolled"
+                >
+                    Rerolled
+                </div>
+            {/if}
         </div>
     {/if}
 </article>
+
+<style>
+    .bnd-heading--chat-title {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .bnd-roll-button {
+            height: 100%;
+            width: fit-content;
+            margin: 0;
+            padding: 0.25rem 0.375rem;
+            background-color: rgba(0, 0, 0, 0.6)
+        }
+    }
+</style>
