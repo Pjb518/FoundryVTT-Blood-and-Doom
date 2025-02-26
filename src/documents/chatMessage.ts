cc26ac776896f9eb9f96a54a45494d78253bf92a
Phil <me@phillip.best>
2025-02-26 20:44:49 +0000

    Implements a new method for rerolling dice when a blood point is spent.

--- src/documents/chatMessage.ts
+++ src/documents/chatMessage.ts
@@ -1,27 +1,82 @@
-export type SystemChatMessageTypes = Exclude<
-  foundry.documents.BaseChatMessage.TypeNames, 'base'
->;
+import type DiceTerm from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client-esm/dice/terms/dice.d.mts';
+
+export type SystemChatMessageTypes = Exclude<foundry.documents.BaseChatMessage.TypeNames, 'base'>;
 
 interface BloodAndDoomChatMessage<
-  ChatMessageType extends SystemChatMessageTypes = SystemChatMessageTypes
+	ChatMessageType extends SystemChatMessageTypes = SystemChatMessageTypes,
 > {
-  type: ChatMessageType;
-  system: DataModelConfig['ChatMessage'][ChatMessageType];
+	type: ChatMessageType;
+	system: DataModelConfig['ChatMessage'][ChatMessageType];
 }
 
 class BloodAndDoomChatMessage extends ChatMessage {
-  /** ------------------------------------------------------ */
-  /**                    Type Helpers                        */
-  /** ------------------------------------------------------ */
-  isType<
-    TypeName extends SystemChatMessageTypes
-  >(type: TypeName): this is BloodAndDoomChatMessage<TypeName> {
-    return type === this.type as SystemChatMessageTypes;
-  }
+	/** ------------------------------------------------------ */
+	/**                    Type Helpers                        */
+	/** ------------------------------------------------------ */
+	isType<TypeName extends SystemChatMessageTypes>(
+		type: TypeName,
+	): this is BloodAndDoomChatMessage<TypeName> {
+		return type === (this.type as SystemChatMessageTypes);
+	}
 
-  /** ------------------------------------------------------ */
-  /**                     Data Prep                          */
-  /** ------------------------------------------------------ */
+	async reroll(): Promise<void> {
+		if (this.type !== 'action') {
+			ui.notifications!.error('Invalid message type. You may only reroll action rolls.');
+			return;
+		}
+
+		if (this.system.setback) {
+			ui.notifications!.error('Cannot reroll a roll with a setback.');
+			return;
+		}
+
+		if (this.system.rerolled) {
+			ui.notifications!.error('Cannot reroll a roll that has already been rerolled.');
+			return;
+		}
+
+		const dice = this.rolls[0]?.dice ?? [];
+		const successes: DiceTerm.Result[] = [];
+
+		for (const dieSet of dice) {
+			for (const result of dieSet.results) {
+				if (result.result > 8) successes.push(result);
+			}
+		}
+
+		const newRoll = new Roll(`${this.system.diceQuantity - successes.length}d10`);
+		await newRoll.evaluate();
+
+		const results = [...newRoll.dice[0].results, ...successes];
+
+		const reroll = Roll.fromTerms([
+			new foundry.dice.terms.Die({ number: this.system.diceQuantity, faces: 10, results }),
+		]);
+
+		const rerollSuccesses = results.reduce((acc, { result }) => {
+			if (result === 10) return acc + 2;
+			if (result >= 8) return acc + 1;
+
+			return acc;
+		}, 0);
+
+		await game.dice3d.showForRoll(
+			reroll,
+			game.user,
+			true,
+			this.whisper,
+			this.blind,
+			null,
+			this.speaker,
+			{ ghost: false, secret: false },
+		);
+
+		this.update({
+			rolls: [reroll],
+			'system.successes': rerollSuccesses,
+			'system.rerolled': true,
+		});
+	}
 }
 
 export { BloodAndDoomChatMessage };
--- src/models/chat/ActionCardDataModel.ts
+++ src/models/chat/ActionCardDataModel.ts
@@ -1,38 +1,52 @@
 const { fields } = foundry.data;
 
 const actionCardSchema = () => ({
-  diceQuantity: new fields.NumberField({
-    required: true, initial: 0, integer: true, nullable: false
-  }),
-  difficulty: new fields.StringField({
-    required: false, initial: "", nullable: true
-  }),
-  setback: new fields.BooleanField({
-    required: true, initial: false, nullable: false
-  }),
-  successes: new fields.NumberField({
-    required: true, initial: 0, integer: true, nullable: false
-  })
+	diceQuantity: new fields.NumberField({
+		required: true,
+		initial: 0,
+		integer: true,
+		nullable: false,
+	}),
+	difficulty: new fields.StringField({
+		required: false,
+		initial: '',
+		nullable: true,
+	}),
+	rerolled: new fields.BooleanField({
+		required: true,
+		initial: false,
+		nullable: false,
+	}),
+	setback: new fields.BooleanField({
+		required: true,
+		initial: false,
+		nullable: false,
+	}),
+	successes: new fields.NumberField({
+		required: true,
+		initial: 0,
+		integer: true,
+		nullable: false,
+	}),
 });
 
 declare namespace BloodAndDoomActionCardData {
-  type Schema = DataSchema
-    & ReturnType<typeof actionCardSchema>;
-  interface BaseData extends Record<string, unknown> { }
-  interface DerivedData extends Record<string, unknown> { }
+	type Schema = DataSchema & ReturnType<typeof actionCardSchema>;
+	interface BaseData extends Record<string, unknown> {}
+	interface DerivedData extends Record<string, unknown> {}
 }
 
 class BloodAndDoomActionCardData extends foundry.abstract.TypeDataModel<
-  BloodAndDoomActionCardData.Schema,
-  ChatMessage.ConfiguredInstance,
-  BloodAndDoomActionCardData.BaseData,
-  BloodAndDoomActionCardData.DerivedData
+	BloodAndDoomActionCardData.Schema,
+	ChatMessage.ConfiguredInstance,
+	BloodAndDoomActionCardData.BaseData,
+	BloodAndDoomActionCardData.DerivedData
 > {
-  static override defineSchema(): BloodAndDoomActionCardData.Schema {
-    return {
-      ...actionCardSchema(),
-    };
-  }
+	static override defineSchema(): BloodAndDoomActionCardData.Schema {
+		return {
+			...actionCardSchema(),
+		};
+	}
 }
 
 export { BloodAndDoomActionCardData };
