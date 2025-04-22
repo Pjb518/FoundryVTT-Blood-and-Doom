775fb8f1188d0b4a16f62a0beb65511851c282ca
Phil <me@phillip.best>
2025-04-22 12:41:36 +0100

    Returns the roll interface to its pre-v13 functionality.

--- src/utils/toggleRollInterfaceTab.ts
+++ src/utils/toggleRollInterfaceTab.ts
@@ -4,11 +4,30 @@ export default function toggleRollInterfaceTab(event) {
   if (!target) return;
 
   const newTab = target.dataset.bndTab;
-  const rollWrapper = document.querySelector(`[data-bnd-wrapper-content="${newTab}"]`);
+  const rollWrapper = document.querySelector(
+    `[data-bnd-wrapper-content="${newTab}"]`,
+  );
 
   if (!rollWrapper) return;
 
-  const chatLogElement = document.querySelector("#chat-log") as HTMLOListElement | null;
+  const tabButtons = document.querySelectorAll("[data-bnd-tab]");
+
+  tabButtons.forEach((button) => {
+    button.classList.remove("bnd-tab-button--active");
+  });
+
+  const rollWrappers = document.querySelectorAll("[data-bnd-wrapper-content");
+
+  rollWrappers.forEach((button) => {
+    button.classList.remove("bnd-roll-button-wrapper--active");
+  });
+
+  target.classList.add("bnd-tab-button--active");
+  rollWrapper.classList.add("bnd-roll-button-wrapper--active");
+
+  const chatLogElement = document.querySelector(
+    ".chat-log",
+  ) as HTMLOListElement | null;
 
   if (!chatLogElement) return;
 
@@ -16,22 +35,11 @@ export default function toggleRollInterfaceTab(event) {
   const initialChatLogScrollHeight = chatLogElement.scrollHeight;
   const initialChatLogScrollLevel = chatLogElement.scrollTop;
 
-  const tabButtons = document.querySelectorAll("[data-bnd-tab]");
-
-  tabButtons.forEach(button => {
-    button.classList.remove("bnd-tab-button--active")
-  })
-
-  const rollWrappers = document.querySelectorAll("[data-bnd-wrapper-content");
-
-  rollWrappers.forEach(button => {
-    button.classList.remove("bnd-roll-button-wrapper--active")
-  })
-
-  target.classList.add("bnd-tab-button--active")
-  rollWrapper.classList.add("bnd-roll-button-wrapper--active");
-
-  if (initialChatLogHeight + initialChatLogScrollLevel !== initialChatLogScrollHeight) return;
+  if (
+    initialChatLogHeight + initialChatLogScrollLevel !==
+    initialChatLogScrollHeight
+  )
+    return;
 
   ui.chat.scrollBottom();
 }
