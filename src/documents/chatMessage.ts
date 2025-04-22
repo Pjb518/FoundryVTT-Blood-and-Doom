5a88158f4d97c74e16098def8aa1e13167b2e2ee
Phil <me@phillip.best>
2025-04-22 13:05:13 +0100

    Adds a guard against DSN not being installed.

--- src/documents/chatMessage.ts
+++ src/documents/chatMessage.ts
@@ -1,85 +1,98 @@
-import type DiceTerm from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client-esm/dice/terms/dice.d.mts';
+import type DiceTerm from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/client-esm/dice/terms/dice.d.mts";
 
-export type SystemChatMessageTypes = Exclude<foundry.documents.BaseChatMessage.TypeNames, 'base'>;
+export type SystemChatMessageTypes = Exclude<
+  foundry.documents.BaseChatMessage.TypeNames,
+  "base"
+>;
 
 interface BloodAndDoomChatMessage<
-	ChatMessageType extends SystemChatMessageTypes = SystemChatMessageTypes,
+  ChatMessageType extends SystemChatMessageTypes = SystemChatMessageTypes,
 > {
-	type: ChatMessageType;
-	system: DataModelConfig['ChatMessage'][ChatMessageType];
+  type: ChatMessageType;
+  system: DataModelConfig["ChatMessage"][ChatMessageType];
 }
 
 class BloodAndDoomChatMessage extends ChatMessage {
-	/** ------------------------------------------------------ */
-	/**                    Type Helpers                        */
-	/** ------------------------------------------------------ */
-	isType<TypeName extends SystemChatMessageTypes>(
-		type: TypeName,
-	): this is BloodAndDoomChatMessage<TypeName> {
-		return type === (this.type as SystemChatMessageTypes);
-	}
+  /** ------------------------------------------------------ */
+  /**                    Type Helpers                        */
+  /** ------------------------------------------------------ */
+  isType<TypeName extends SystemChatMessageTypes>(
+    type: TypeName,
+  ): this is BloodAndDoomChatMessage<TypeName> {
+    return type === (this.type as SystemChatMessageTypes);
+  }
 
-	async reroll(): Promise<void> {
-		if (this.type !== 'action') {
-			ui.notifications!.error('Invalid message type. You may only reroll action rolls.');
-			return;
-		}
+  async reroll(): Promise<void> {
+    if (this.type !== "action") {
+      ui.notifications!.error(
+        "Invalid message type. You may only reroll action rolls.",
+      );
+      return;
+    }
 
-		if (this.system.setback) {
-			ui.notifications!.error('Cannot reroll a roll with a setback.');
-			return;
-		}
+    if (this.system.setback) {
+      ui.notifications!.error("Cannot reroll a roll with a setback.");
+      return;
+    }
 
-		if (this.system.rerolled) {
-			ui.notifications!.error('Cannot reroll a roll that has already been rerolled.');
-			return;
-		}
+    if (this.system.rerolled) {
+      ui.notifications!.error(
+        "Cannot reroll a roll that has already been rerolled.",
+      );
+      return;
+    }
 
-		const dice = this.rolls[0]?.dice ?? [];
-		const successfulDiceResults: DiceTerm.Result[] = [];
+    const dice = this.rolls[0]?.dice ?? [];
+    const successfulDiceResults: DiceTerm.Result[] = [];
 
-		for (const dieSet of dice) {
-			for (const result of dieSet.results) {
-				if (result.result >= 8) successfulDiceResults.push(result);
-			}
-		}
+    for (const dieSet of dice) {
+      for (const result of dieSet.results) {
+        if (result.result >= 8) successfulDiceResults.push(result);
+      }
+    }
 
-		const newRoll = new Roll(`${this.system.diceQuantity - successfulDiceResults.length}d10`);
-		await newRoll.evaluate();
+    const newRoll = new Roll(
+      `${this.system.diceQuantity - successfulDiceResults.length}d10`,
+    );
+    await newRoll.evaluate();
 
-		const results = [...newRoll.dice[0].results, ...successfulDiceResults];
+    const results = [...newRoll.dice[0].results, ...successfulDiceResults];
 
-		const reroll = Roll.fromTerms([
-			new foundry.dice.terms.Die({ number: this.system.diceQuantity, faces: 10, results }),
-		]);
+    const reroll = Roll.fromTerms([
+      new foundry.dice.terms.Die({
+        number: this.system.diceQuantity,
+        faces: 10,
+        results,
+      }),
+    ]);
 
-		const rerollSuccesses = results.reduce((acc, { result }) => {
-			if (result === 10) return acc + 2;
-			if (result >= 8) return acc + 1;
+    const rerollSuccesses = results.reduce((acc, { result }) => {
+      if (result === 10) return acc + 2;
+      if (result >= 8) return acc + 1;
 
-			return acc;
-		}, 0);
+      return acc;
+    }, 0);
 
-		game.dice3d.showForRoll(
-			newRoll,
-			game.user,
-			true,
-			this.whisper,
-			this.blind,
-			null,
-			this.speaker,
-			{
-				ghost: false,
-				secret: false,
-			},
-		);
+    game.dice3d?.showForRoll(
+      newRoll,
+      game.user,
+      true,
+      this.whisper,
+      this.blind,
+      null,
+      this.speaker,
+      {
+        ghost: false,
+        secret: false,
+      },
+    );
 
-		this.update({
-			rolls: [reroll],
-			'system.successes': rerollSuccesses,
-			'system.rerolled': true,
-		});
-	}
+    this.update({
+      rolls: [reroll],
+      "system.successes": rerollSuccesses,
+      "system.rerolled": true,
+    });
+  }
 }
 
 export { BloodAndDoomChatMessage };
